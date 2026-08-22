444c87e79915b7e3cb65051108b4fb9345a3bece
Anna Shahed <annashahed09@gmail.com>
2026-08-22 01:34:29 -0400

    Fix formatting in as_spectrogram_colorscale function

--- src/signal_processing/visualization/theme.py
+++ src/signal_processing/visualization/theme.py
@@ -106,7 +106,6 @@ def new_plotly_figure() -> go.Figure:
     fig = go.Figure()
     fig.update_layout(template=plotly_template())
     return fig
-
 def as_spectrogram_colorscale() -> list[list[float | str]]:
     n = len(SPECTROGRAM_CMAP)
     return [[i / (n - 1), color] for i, color in enumerate(SPECTROGRAM_CMAP)]
