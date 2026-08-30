352a97afe3af89b3bd8009850f30d13f9848e75f
Anna Shahed <annashahed09@gmail.com>
2026-08-30 18:49:48 -0400

    Add SignalIOError exception class

--- src/signal_processing/core/__init__.py
+++ src/signal_processing/core/__init__.py
@@ -4,13 +4,16 @@ from .signal import Signal
 from .spectrogram import Spectrogram
 from .spectrum import Spectrum
 
+
+class SignalIOError(Exception):
+    """Raised when a signal cannot be read from or written to a file."""
+
+
 __all__ = [
     "AnalysisResult",
     "Event",
     "Signal",
+    "SignalIOError",
     "Spectrogram",
     "Spectrum",
 ]
-class SignalIOError(Exception):
-    """Raised when a signal cannot be read from or written to a file."""
-
